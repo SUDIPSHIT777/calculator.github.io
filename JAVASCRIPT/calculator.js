22a3b8bfdab9a6b0b92b88e236cdab97c3ff90a1
SUDIPSHIT777 <sudipshit777@gmail.com>
2025-06-20 22:13:15 +0530

    Calculator

---
+++ JAVASCRIPT/calculator.js
@@ -0,0 +1,55 @@
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input3');
+const symbol = document.getElementById('symbols');
+const Displayresult = document.getElementById('showresult');
+document.getElementById("clear").addEventListener('click', () => {
+    input1.value = ''
+    symbol.value = ''
+    input2.value = ''
+    Displayresult.innerText = '';
+})
+
+document.getElementById("mod").addEventListener('click', () => { symbol.value = "%" });
+document.getElementById("devesion").addEventListener('click', () => { symbol.value = "/" })
+document.getElementById("multiply").addEventListener('click', () => { symbol.value = "*" })
+document.getElementById("minus").addEventListener('click', () => { symbol.value = "-" })
+document.getElementById("plus").addEventListener('click', () => { symbol.value = "+" })
+
+function up1() {
+   if(isNaN(input1.value) || isNaN(input2.value) || isNaN(symbol.value)){
+        input1.value = ''
+        input2.value = ''
+        symbol.value = ''
+   }
+}
+
+function RESULT() {
+    const num1 = parseFloat(input1.value);
+    const num2 = parseFloat(input2.value);
+    const calculate = symbol.value.trim();
+    let result;
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Enter Value To be Calculate")
+    }
+    switch (calculate) {
+        case "+":
+            result = num1 + num2
+            break;
+        case "-":
+            result = num1 - num2
+            break;
+        case "%":
+            result = num1 % num2
+            break;
+        case "/":
+            result = num1 / num2
+            break;
+        case "*":
+            result = num1 * num2
+            break;
+        default:
+            result = "Invalid Operator"
+    }
+    Displayresult.textContent = result;
+
+}
---
+++ JAVASCRIPT/test1.js
@@ -0,0 +1,34 @@
+let username = "Sudip Shit "
+// console.log(username.charAt(0));//chart is find the index of a string ;
+
+//.indexof method in returns the value of a index
+// console.log(username.indexOf("i"))
+
+//.lastindexOf in returns the value of a index i last order
+// console.log(username.lastIndexOf(username));
+
+//.trim(this should be void)
+// username = username.trim()
+// console.log(username);
+
+//.repeat(3) repeat the string 
+// username = username.repeat(2)
+// console.log(username)
+
+//.startwith() and .endswith() return boolean
+// result = username.startsWith("S")
+// r = username.endsWith("S")
+// console.log(result,r)
+
+//.replaceAll("-") what should be replace
+// let phonenum = "967-937-131-0"
+// re = phonenum.replaceAll("-", "")//search value and replace value
+// console.log(re)
+
+//.padstrats the value and add
+// let phonenum = "967-937-131-0"
+// let replace = phonenum.replaceAll("-", "")
+// let re = replace.padStart(14, "+91 ")
+// console.log(re);
+
+
