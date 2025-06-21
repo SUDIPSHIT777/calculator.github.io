011f73e95c680eb9c0e9774b3ab705bb9e056599
SUDIPSHIT777 <sudipshit777@gmail.com>
2025-06-21 10:33:37 +0530

    calculator.js

--- JAVASCRIPT/calculator.js
+++ JAVASCRIPT/calculator.js
@@ -4,16 +4,16 @@ const symbol = document.getElementById('symbols');
 const Displayresult = document.getElementById('showresult');
 document.getElementById("clear").addEventListener('click', () => {
     input1.value = ''
-    symbol.value = ''
     input2.value = ''
+    symbol.innerText = ''
     Displayresult.innerText = '';
 })
 
-document.getElementById("mod").addEventListener('click', () => { symbol.value = "%" });
-document.getElementById("devesion").addEventListener('click', () => { symbol.value = "/" })
-document.getElementById("multiply").addEventListener('click', () => { symbol.value = "*" })
-document.getElementById("minus").addEventListener('click', () => { symbol.value = "-" })
-document.getElementById("plus").addEventListener('click', () => { symbol.value = "+" })
+document.getElementById("mod").addEventListener('click', () => { symbol.textContent = "%" });
+document.getElementById("devesion").addEventListener('click', () => { symbol.textContent = "/" })
+document.getElementById("multiply").addEventListener('click', () => { symbol.textContent = "*" })
+document.getElementById("minus").addEventListener('click', () => { symbol.textContent = "-" })
+document.getElementById("plus").addEventListener('click', () => { symbol.textContent = "+" })
 
 function up1() {
     if (isNaN(input1.value)) {
@@ -27,18 +27,11 @@ function up3() {
     }
 }
 
-function up2() {
-    if (isNaN(symbol.value)) {
-        symbol.value = ""
-        Displayresult.innerHTML = '<p style="font-size: 15px;">Symbol must be input on calculator</p>';
-    }
-
-}
 
 function RESULT() {
     const num1 = parseFloat(input1.value);
     const num2 = parseFloat(input2.value);
-    const calculate = symbol.value.trim();
+    const calculate = symbol.textContent.trim();
     let result;
     if (isNaN(num1) || isNaN(num2)) {
         alert("Enter Value To be Calculate")
